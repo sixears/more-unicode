7389438fcfb2beb3281ed00403a0a059203375ad
Mr. Fluffy <git@sixearsm>
2019-04-19 13:13:16 +0000

    more lens symbols

--- src/Data/MoreUnicode/Lens.hs
+++ src/Data/MoreUnicode/Lens.hs
@@ -1,21 +1,39 @@
-{-# LANGUAGE UnicodeSyntax #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE UnicodeSyntax    #-}
 
 module Data.MoreUnicode.Lens
-  ( (⊣), (⊢), (⩼), (##) )
+  ( (⊣), (⊥), (⊢), (⩼), (##), (⋖), (⋗), tindex )
 where
+
+import Prelude  ( Int, fromIntegral )
 
 -- base --------------------------------
 
-import Data.Function  ( flip )
-import Data.Maybe     ( Maybe )
-import Data.Monoid    ( First )
+import Control.Applicative  ( Applicative )
+import Data.Function        ( flip )
+import Data.Maybe           ( Maybe )
+import Data.Monoid          ( First )
+import Data.Traversable     ( Traversable )
+
+-- base-unicode-symbols ----------------
+
+import Data.Function.Unicode  ( (∘) )
 
 -- lens --------------------------------
 
-import Control.Lens.Fold    ( (^?) )
-import Control.Lens.Getter  ( Getting, (^.) )
-import Control.Lens.Review  ( AReview, (#) )
-import Control.Lens.Setter  ( ASetter, (.~) )
+import Control.Lens.Cons       ( Cons, Snoc, (|>), (<|) )
+import Control.Lens.Fold       ( (^?) )
+import Control.Lens.Getter     ( Getting, (^.) )
+import Control.Lens.Indexed    ( Indexable, index )
+import Control.Lens.Review     ( AReview, (#) )
+import Control.Lens.Setter     ( ASetter, (.~) )
+import Control.Lens.Traversal  ( traversed )
+
+------------------------------------------------------------
+--                     local imports                      --
+------------------------------------------------------------
+
+import Data.MoreUnicode.Natural  ( ℕ )
 
 --------------------------------------------------------------------------------
 
@@ -34,5 +52,23 @@ infixl 8 ⩼
 infixr 8 ##
 (##) ∷ α → AReview δ α → δ
 (##) = flip (#)
+
+{-| index into a traversable thing) -}
+tindex ∷ (Indexable Int ι, Traversable ψ, Applicative ξ) =>
+         ((ψ α -> ξ (ψ α)) -> β) -> ℕ -> ι α (ξ α) -> β
+
+tindex l i = l ∘ traversed ∘ index (fromIntegral i)
+
+infixr 8 ⊥
+(⊥) ∷ (Indexable Int ι, Traversable ψ, Applicative ξ) =>
+      ((ψ α -> ξ (ψ α)) -> β) -> ℕ -> ι α (ξ α) -> β
+(⊥) = tindex
+
+----------------------------------------
+
+(⋖) ∷ Cons σ σ α α => α -> σ -> σ 
+(⋖) = (<|)
+(⋗) ∷ Snoc σ σ α α => σ -> α -> σ 
+(⋗) = (|>)
 
 -- that's all, folks! ----------------------------------------------------------
