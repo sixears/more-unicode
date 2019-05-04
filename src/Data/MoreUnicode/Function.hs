c37ae9ac48eeb5d11cd8e2c517b58e66455ba54e
Mr. Fluffy <git@sixearsm>
2019-05-04 06:47:54 +0000

    fix precedence for lower-precedence-reverse-apply (⅋)

--- src/Data/MoreUnicode/Function.hs
+++ src/Data/MoreUnicode/Function.hs
@@ -12,7 +12,7 @@ import Data.Function   ( (&) )
 
 -------------------------------------------------------------------------------
 
-infixl 1 ⅋
+infixl 3 ⅋
 {-| a version of `(&)` (reverse-application) with higher precedence (3) to allow
     it to more easily be used with, e.g., `(≟)` which has a precedence of 1
 -}
