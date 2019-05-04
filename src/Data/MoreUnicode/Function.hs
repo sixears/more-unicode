d9233577cb614260561bbd879e9b6c5768d29419
Mr. Fluffy <git@sixearsm>
2019-05-04 06:36:41 +0000

    add lower-precedence reverse-apply (⅋)

---
+++ src/Data/MoreUnicode/Function.hs
@@ -0,0 +1,22 @@
+{-# LANGUAGE UnicodeSyntax #-}
+
+module Data.MoreUnicode.Function
+  ( (⅋) )
+where
+
+import Prelude ()
+
+-- base --------------------------------
+
+import Data.Function   ( (&) )
+
+-------------------------------------------------------------------------------
+
+infixl 1 ⅋
+{-| a version of `(&)` (reverse-application) with higher precedence (3) to allow
+    it to more easily be used with, e.g., `(≟)` which has a precedence of 1
+-}
+(⅋) ∷ α → (α → β) → β
+(⅋) = (&)
+
+-- that's all, folks! ---------------------------------------------------------
