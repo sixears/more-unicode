0fe103f16ed0d0ebcb2cf06c0176e3bb9941957f
Mr. Fluffy <git@sixearsm>
2019-08-23 12:58:36 +0000

    documentation

--- src/Data/MoreUnicode/MonoTraversable.hs
+++ src/Data/MoreUnicode/MonoTraversable.hs
@@ -13,10 +13,13 @@ import Data.MonoTraversable  ( Element, MonoFunctor( omap ) )
 -------------------------------------------------------------------------------
 
 infixl 4 ⪧
+{- | alias for `omap`, that is, `Data.Functor.fmap` over a monofunctor -}
 (⪧) ∷ MonoFunctor ψ ⇒ (Element ψ → Element ψ) → ψ → ψ
 (⪧) = omap
 
 -- | flip `(⪧)`
+{- | flipped alias for `omap`, that is, `Data.Functor.fmap` over a monofunctor
+ -}
 infixl 4 ⪦
 (⪦) ∷ MonoFunctor ψ ⇒ ψ → (Element ψ → Element ψ) → ψ
 as ⪦ f = omap f as
--- src/Data/MoreUnicode/Semigroup.hs
+++ src/Data/MoreUnicode/Semigroup.hs
@@ -10,6 +10,7 @@ import Data.Semigroup  ( Semigroup, (<>) )
 
 --------------------------------------------------------------------------------
 
+{- | alias for `(<>)`, that is, `Data.Monoid.mappend` for semigroups -}
 (◇) ∷ Semigroup α ⇒ α → α → α
 (◇) = (<>)
 
