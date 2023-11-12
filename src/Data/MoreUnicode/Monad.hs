d4a0b360e69ab3bd45f2bbba0a83b34a53f93f26
Mr. Fluffy <git@sixearsm>
2023-11-12 15:16:04 +0000

    r0.0.19.0

--- src/Data/MoreUnicode/Monad.hs
+++ src/Data/MoreUnicode/Monad.hs
@@ -1,12 +1,27 @@
+{-# LANGUAGE UnicodeSyntax #-}
 module Data.MoreUnicode.Monad
-  ( (⪻), (⪼), (≪), (≫), (⋘), (⋙), (⩤), (⩥) )
-where
+  ( (≪)
+  , (≫)
+  , (⋘)
+  , (⋙)
+  , (⩤)
+  , (⩥)
+  , (⪻)
+  , (⪼)
+  , (⮘)
+  , (⮚)
+  , (⮜)
+  , (⮞)
+  ) where
 
 -- base --------------------------------
 
-import Control.Monad  ( Monad, (>>), (>>=), (=<<), (<=<), (>=>) )
-import Data.Function  ( flip )
-import Data.Functor   ( Functor, fmap )
+import Control.Monad    ( Monad, forM, forM_, mapM, mapM_, (<=<), (=<<), (>=>),
+                          (>>), (>>=) )
+import Data.Foldable    ( Foldable )
+import Data.Function    ( flip )
+import Data.Functor     ( Functor, fmap )
+import Data.Traversable ( Traversable )
 
 --------------------------------------------------------------------------------
 
@@ -30,7 +45,7 @@ infixr 1 ≪
 (≪) ∷ Monad η ⇒ (α → η β) → η α → η β
 (≪) = (=<<)
 
--- | unicode alias for `(<=<)`
+-- | unicode alias for `(>=>)`
 infixr 1 ⋙
 (⋙) ∷ Monad η ⇒ (α → η β) → (β → η γ) → α → η γ
 (⋙) = (>=>)
@@ -40,7 +55,7 @@ infixr 1 ⋘
 (⋘) ∷ Monad η ⇒ (β → η γ) → (α → η β) → α → η γ
 (⋘) = (<=<)
 
-{- | A bit like `(=<<)` / `(⊲)`, but allows the rhs to be a function itself
+{- | A bit like `(=<<)` / `(⊲)`, but allows the lhs to be a function itself
      for point-free styling.
 -}
 (⩤) ∷ (Monad η, Functor ψ) ⇒ (α → η β) → ψ (η α) → ψ (η β)
@@ -51,5 +66,21 @@ x ⩤ y = fmap (x ≪) y
 -}
 (⩥) ∷ (Monad η, Functor ψ) ⇒ ψ (η α) → (α → η β) → ψ (η β)
 x ⩥ y = fmap (≫ y) x
+
+{-| unicode alias for `mapM` -}
+(⮞) ∷ (Monad η, Traversable ψ) ⇒ (α → η β) → ψ α → η (ψ β)
+(⮞) = mapM
+
+{-| unicode alias for `mapM_` -}
+(⮚) ∷ (Monad η, Foldable φ) ⇒ (α → η ()) → φ α → η ()
+(⮚) = mapM_
+
+{-| unicode alias for `forM` -}
+(⮜) ∷ (Monad η, Traversable ψ) ⇒ ψ α → (α → η β) → η (ψ β)
+(⮜) = forM
+
+{-| unicode alias for `forM_` -}
+(⮘) ∷ (Monad η, Foldable φ) ⇒ φ α → (α → η ()) → η ()
+(⮘) = forM_
 
 -- that's all, folks! ----------------------------------------------------------
